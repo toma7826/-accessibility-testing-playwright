92b9471e93218b1bb86581b1d55fa38af7c6ae35
Akhil Tom <toma@roehampton.ac.uk>
2025-08-04 21:53:18 +0530

    Update accessibility audit reports and code

--- tests/accessibility-check.spec.ts
+++ tests/accessibility-check.spec.ts
@@ -3,31 +3,31 @@ import AxeBuilder from '@axe-core/playwright';
 import fs from 'fs';
 import path from 'path';
 
-// 20 websites to test
+// 23 websites to test
 const websites = [
-  { name: 'BBC', url: 'https://www.bbc.com' },
+  { name: 'BBC', url: 'https://www.bbc.co.uk' },
   { name: 'NHS', url: 'https://www.nhs.uk' },
-  { name: 'YouTube', url: 'https://www.youtube.com' },
+  { name: 'Scope', url: 'https://www.scope.org.uk' },
   { name: 'GOVUK', url: 'https://www.gov.uk' },
-  { name: 'Netflix', url: 'https://www.netflix.com/gb/' },
-  { name: 'Scope', url: 'https://www.scope.org.uk/' },
-  { name: 'AbilityNet', url: 'https://abilitynet.org.uk/' },
-  { name: 'TED', url: 'https://www.ted.com/' },
-  { name: 'Wikipedia', url: 'https://www.wikipedia.org/' },
-  { name: 'WebAIM', url: 'https://webaim.org/' },
-  { name: 'RNIB', url: 'https://www.rnib.org.uk/' },
-  { name: 'Spotify', url: 'https://www.spotify.com/uk/' },
-  { name: 'Zoom', url: 'https://zoom.us/' },
-  { name: 'Facebook', url: 'https://www.facebook.com/' },
-  { name: 'LinkedIn', url: 'https://www.linkedin.com/' },
-  { name: 'Reddit', url: 'https://www.reddit.com/' },
-  { name: 'Apple', url: 'https://www.apple.com/uk/' },
-  { name: 'Google Search', url: 'https://www.google.com/' },
-  { name: 'Pinterest', url: 'https://www.pinterest.co.uk/' },
-  { name: 'Coursera', url: 'https://www.coursera.org/' },
-  { name: 'MS Team', url: 'https://www.microsoft.com/en-gb/microsoft-teams/group-chat-software' },
-  { name: 'Open Office', url: 'https://www.openoffice.org/' },
-  { name: 'Share Drive', url: 'https://sharedrive.co/' }
+  { name: 'Spotify', url: 'https://www.spotify.com/uk' },
+  { name: 'YouTube', url: 'https://www.youtube.com' },
+  { name: 'Zoom', url: 'https://zoom.us' },
+  { name: 'Apple', url: 'https://www.apple.com/uk' },
+  { name: 'Facebook', url: 'https://www.facebook.com' },
+  { name: 'WebAIM', url: 'https://webaim.org' },
+  { name: 'AgeUK', url: 'https://www.ageuk.org.uk' },
+  { name: 'TED', url: 'https://www.ted.com' },
+  { name: 'WebMD', url: 'https://www.webmd.com' },
+  { name: 'Pinterest', url: 'https://www.pinterest.co.uk' },
+  { name: 'Shelter', url: 'https://www.shelter.org.uk' },
+  { name: 'Autism UK', url: 'https://www.autism.org.uk' },
+  { name: 'Mencap', url: 'https://www.mencap.org.uk' },
+  { name: 'Microsoft Teams', url: 'https://www.microsoft.com/en/microsoft-teams/group-chat-software' },
+  { name: 'OpenOffice', url: 'https://www.openoffice.org' },
+  { name: 'SharePoint', url: 'https://www.microsoft.com/en-ww/microsoft-365/sharepoint/collaboration' },
+  { name: 'Google', url: 'https://www.google.co.uk' },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com' },
+  { name: 'Reddit', url: 'https://www.reddit.com' }
 ];
 
 test.describe('Accessibility Tests', () => {
